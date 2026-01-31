d57e1e7f0612836a76dbfabe6261b5169c3f0e01
Payal Ojha <payalojha2004@gmail.com>
2026-01-31 16:44:44 +0530

    changed properties

---
+++ src/main/java/com/shortener/URLShortener/GenerateSecret.java
@@ -0,0 +1,13 @@
+package com.shortener.URLShortener;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class GenerateSecret {
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[32];
+        random.nextBytes(bytes);
+        System.out.println(Base64.getEncoder().encodeToString(bytes));
+    }
+}
